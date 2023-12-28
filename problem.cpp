c8b1d7352e7a61e274506b25992ec4fd84a56853
Nandeesh Bhatrai <ninad.bhatrai1@gmail.com>
2023-12-28 22:35:33 +0530

    solved contest problems => Codeforces Round 918 (Div. 4)

--- problem.cpp
+++ problem.cpp
@@ -1,139 +1,69 @@
-#include <bits/stdc++.h>
-#include <chrono>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
-using namespace std;
-using namespace std::chrono;
+class FenwickTree {
+public:
+    FenwickTree(int size) : size(size), tree(size + 1, 0) {}
 
-#pragma GCC target ("avx2")
-#pragma GCC optimization ("O3")
-#pragma GCC optimization ("unroll-loops")
-#pragma optimization_level 3
-#pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,fast-math,O3")
-#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
-
-#define f0r(a, b) for (long long a = 0; a < (b); ++a)
-#define f1r(a, b, c) for (long long a = (b); a < (c); ++a)
-#define f0rd(a, b) for (long long a = (b); a >= 0; --a)
-#define f1rd(a, b, c) for (long long a = (b); a >= (c); --a)
-#define ms(arr, v) memset(arr, v, sizeof(arr))
-#define pb push_back
-#define send {ios_base::sync_with_stdio(false);}
-#define help {cin.tie(NULL); cout.tie(NULL);}
-#define fix(prec) {cout << setprecision(prec) << fixed;}
-#define mp make_pair
-#define f first
-#define s second
-#define all(v) v.begin(), v.end()
-#define getunique(v) {sort(all(v)); v.erase(unique(all(v)), v.end());}
-#define readgraph(list, edges) for (int i = 0; i < edges; i++) {int a, b; cin >> a >> b; a--; b--; list[a].pb(b); list[b].pb(a);}
-#define ai(a, n) for (int ele = 0; ele < n; ele++) cin >> a[ele];
-#define ain(a, lb, rb) for (int ele = lb; ele <= rb; ele++) cin >> a[ele];
-#define ao(a, n) {for (int ele = 0; ele < (n); ele++) { if (ele) cout << " "; cout << a[ele]; } cout << '\n';}
-#define aout(a, lb, rb) {for (int ele = (lb); ele <= (rb); ele++) { if (ele > (lb)) cout << " "; cout << a[ele]; } cout << '\n';}
-#define vsz(x) ((long long) x.size())
-#define nahi {cout << "NO" << '\n';}
-#define haa {cout << "YES" << '\n';}
-typedef long long ll;
-typedef long double lld;
-typedef unsigned long long ull;
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
-typedef vector<int> vi;
-typedef vector<ll> vl;
-typedef vector<pii> vpi;
-typedef vector<pll> vpl;
-
-template<typename A> ostream& operator<<(ostream &cout, vector<A> const &v);
-template<typename A, typename B> ostream& operator<<(ostream &cout, pair<A, B> const &p) { return cout << "(" << p.f << ", " << p.s << ")"; }
-template<typename A> ostream& operator<<(ostream &cout, vector<A> const &v) {
-    cout << "["; for(int i = 0; i < v.size(); i++) {if (i) cout << ", "; cout << v[i];} return cout << "]";
-}
-template<typename A, typename B> istream& operator>>(istream& cin, pair<A, B> &p) {
-    cin >> p.first;
-    return cin >> p.second;
-}
-template<typename K, typename V> ostream& operator<<(ostream& cout, const map<K, V>& m) {
-    cout << "{ ";
-    for (auto it = m.begin(); it != m.end(); ++it) {
-        if (it != m.begin())
-            cout << ", ";
-        cout << '"' << it->first << '"' << ": " << '"' << it->second << '"';
-    }
-    cout << " }";
-    return cout;
-}
-
-bool prime(ll n) {
-    if (n <= 1) {return false;}
-    if (n == 2 || n == 3) {return true;}
-    if (n % 2 == 0 || n % 3 == 0) {return false;}
-    for (ll i = 5; i * i <= n; i += 6) {
-        if ((n % i == 0 || n % (i + 2) == 0)) {return false;}
-    }
-    return true;
-}
-
-void printprimefactors(ll n) {
-    for (int i = 1; i * i <= n; ++i) {
-        if (n % i == 0) {
-            cout << i << ' ';
+    void update(int index, int value) {
+        while (index <= size) {
+            tree[index] += value;
+            index += index & -index;
         }
     }
-    for (int i = sqrt(n) + 1; i > 0; --i) {
-        if (n % i == 0) {
-            cout << n / i << ' ';
+
+    int query(int index) {
+        int result = 0;
+        while (index > 0) {
+            result += tree[index];
+            index -= index & -index;
         }
+        return result;
     }
-}
 
-vector<bool> sieve(ll n) {
-    vector<bool> primes(n + 1, true);
-    primes[0] = primes[1] = false;
-    for (ll index = 2; index * index <= n; ++index) {
-        if (primes[index]) {
-            for (ll i = index * index; i <= n; i += index) {
-                primes[i] = false;
-            }
-        }
+private:
+    int size;
+    std::vector<int> tree;
+};
+
+std::vector<int> countSmallerValuesAfter(const std::vector<int>& arr) {
+    int n = arr.size();
+    std::vector<std::pair<int, int>> indexedArr(n);
+    for (int i = 0; i < n; ++i) {
+        indexedArr[i] = {i, arr[i]};
     }
-    return primes;
-}
 
-// const lld pi = 3.14159265358979323846;
-// const ll mod = 1000000007;
-// const ll mod = 998244353;
-// ll mod;
+    std::sort(indexedArr.begin(), indexedArr.end(), [](const auto& a, const auto& b) {
+        return a.second < b.second;
+    });
 
-ll n, m, k, p, q, l, r, w, x, y, z, h, d;
-// const ll template_array_size = 1e6 + 585;
-// ll a[template_array_size];
-// ll b[template_array_size];
-// ll c[template_array_size];
-string s;
-ll ans = 0;
+    FenwickTree fenwickTree(n);
+    std::vector<int> result(n, 0);
 
-void solve(int tc = 0){
-    // Your solution goes here
-    
+    for (int i = 0; i < n; ++i) {
+        result[indexedArr[i].first] = fenwickTree.query(n) - fenwickTree.query(indexedArr[i].first);
+        fenwickTree.update(indexedArr[i].first + 1, 1);
+    }
+
+    return result;
 }
 
 int main() {
-    #ifdef kali㉿Nandeesh
-        auto begin = std::chrono::high_resolution_clock::now();
-    #endif
+    std::vector<int> a = {4, 2, 9, 7, 5};
+    std::vector<int> b = countSmallerValuesAfter(a);
 
-    send help
+    std::cout << "Array a:";
+    for (int num : a) {
+        std::cout << " " << num;
+    }
+    std::cout << std::endl;
 
-    // Uncomment the next line if you are using USACO
-    // usaco("filename");
+    std::cout << "Array b:";
+    for (int num : b) {
+        std::cout << " " << num;
+    }
+    std::cout << std::endl;
 
-    int tc = 1;
-    cin >> tc;
-    for (int t = 0; t < tc; t++) solve(t);
-
-    #ifdef kali㉿Nandeesh
-        auto end = std::chrono::high_resolution_clock::now();
-        cout << setprecision(14) << fixed;
-        cout << "Execution time: " << std::chrono::duration_cast<std::chrono::duration<double>>(end - begin).count() << " seconds" << endl;
-    #endif
+    return 0;
 }
--- problem.py
+++ problem.py
@@ -1,72 +1,34 @@
-import sys
-import io
-import os
-import math
-from typing import List, Tuple
+class FenwickTree:
+    def __init__(self, size):
+        self.size = size
+        self.tree = [0] * (size + 1)
 
-# Disable buffering for interactive problems
-input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
+    def update(self, index, value):
+        while index <= self.size:
+            self.tree[index] += value
+            index += index & -index
 
-# Uncomment the following lines if you need to use external C++ code
-# import subprocess
-# subprocess.run(['g++', '-O3', 'external_code.cpp', '-o', 'external_code'])
+    def query(self, index):
+        result = 0
+        while index > 0:
+            result += self.tree[index]
+            index -= index & -index
+        return result
 
-# Common optimization flags for GCC
-# You can adjust these based on your needs
-# flags = ['-O3', '-march=native', '-funroll-loops', '-fno-stack-protector', '-fno-strict-aliasing']
-# os.environ['CXXFLAGS'] = '- '.join(flags)
+def count_smaller_values_after(arr):
+    n = len(arr)
+    sorted_arr = sorted(enumerate(arr), key=lambda x: x[1])
+    fenwick_tree = FenwickTree(n)
+    result = [0] * n
 
-def prime(n: int) -> bool:
-    if n <= 1:
-        return False
-    if n == 2 or n == 3:
-        return True
-    if n % 2 == 0 or n % 3 == 0:
-        return False
-    for i in range(5, int(math.sqrt(n)) + 1, 6):
-        if n % i == 0 or n % (i + 2) == 0:
-            return False
-    return True
+    for i, (_, value) in enumerate(sorted_arr):
+        result[sorted_arr[i][0]] = fenwick_tree.query(n) - fenwick_tree.query(sorted_arr[i][0])
+        fenwick_tree.update(sorted_arr[i][0] + 1, 1)
 
-def print_prime_factors(n: int) -> None:
-    for i in range(1, int(math.sqrt(n)) + 1):
-        if n % i == 0:
-            print(i, end=' ')
-    for i in range(int(math.sqrt(n)) + 1, 0, -1):
-        if n % i == 0:
-            print(n // i, end=' ')
+    return result
 
-def sieve(n: int) -> List[bool]:
-    primes = [True] * (n + 1)
-    primes[0] = primes[1] = False
-    for index in range(2, int(math.sqrt(n)) + 1):
-        if primes[index]:
-            for i in range(index * index, n + 1, index):
-                primes[i] = False
-    return primes
-
-def solve() -> None:
-    # Your solution goes here
-    pass
-
-def main() -> None:
-    # Uncomment the next line if you are using USACO
-    # usaco("filename")
-
-    # Common input/output optimizations
-    # sys.set_in(input)
-    # sys.stdout = io.BytesIO()
-
-    t = int(input().decode())
-    for _ in range(t):
-        solve()
-
-if __name__ == "__main__":
-    # Uncomment the following lines if you want to measure execution time
-    # import time
-    # start_time = time.time()
-
-    main()
-
-    # Uncomment the following lines if you want to measure execution time
-    # print("Execution time:", time.time() - start_time, "seconds")
+# Example usage:
+a = [4, 2, 9, 7, 5]
+b = count_smaller_values_after(a)
+print("Array a:", a)
+print("Array b:", b)
