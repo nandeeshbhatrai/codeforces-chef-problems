ad862c81806814f0003de7f19d1212e32a44ac4e
Nandeesh Bhatrai <official.nandeeshbhatrai@gmail.com>
2024-11-09 20:04:01 +0530

    added a few sols

---
+++ template/plain.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,fast-math,O3")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#define send {ios_base::sync_with_stdio(false);}
+#define help {cin.tie(NULL); cout.tie(NULL);}
+
+typedef long long ll;
+typedef vector<ll> vl;
+#define f first
+#define s second
+#define pb push_back
+
+template<typename A> ostream& operator<<(ostream &cout, deque<A> const &v) {
+    cout << "["; for(int i = 0; i < v.size(); i++) {if (i) cout << ", "; cout << v[i];} return cout << "]";
+}
+
+const ll mod = 1000000007;
+// const ll mod = 998244353;
+ll n, m, k, p, q, l, r, w, x, y, z , h, d;
+string s;
+ll ans = 0;
+
+void solve(){
+    
+}
+
+int main(){
+    send help
+    ll tc = 1;
+    cin >> tc;
+    while(tc--){
+        solve();
+    }
+}
